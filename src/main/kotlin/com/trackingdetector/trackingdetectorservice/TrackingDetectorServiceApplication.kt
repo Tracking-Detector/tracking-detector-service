5ba26fb7bf59d567ec8143b2603263cc70dff72c
heschwerdt <henry.schwerdtner@web.de>
2023-03-27 09:13:46 +0200

    initial commit

---
+++ src/main/kotlin/com/trackingdetector/trackingdetectorservice/TrackingDetectorServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.trackingdetector.trackingdetectorservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrackingDetectorServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrackingDetectorServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/trackingdetector/trackingdetectorservice/TrackingDetectorServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.trackingdetector.trackingdetectorservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrackingDetectorServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
